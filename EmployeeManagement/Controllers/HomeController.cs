e7890a43e346b935cdaa125824d87209c95cd0ff
Bipin Paul <b9812398@gmail.com>
2020-06-30 16:14:49 +0530

    Add project files.

---
+++ EmployeeManagement/Controllers/HomeController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using EmployeeManagement.Models;
+using EmployeeManagement.ViewModels;
+
+namespace EmployeeManagement.Controllers
+{
+    
+    public class HomeController : Controller
+    {
+        private IEmployeeRepository _employeeRepository;
+        public HomeController(IEmployeeRepository employeeRepository)
+        {
+            _employeeRepository = employeeRepository;
+        }
+
+        
+        public ViewResult Index()
+        {
+            // retrieve all the employees
+            var model = _employeeRepository.GetAllEmployee();
+            // Pass the list of employees to the view
+            return View(model);
+        }
+
+        
+        public ViewResult Details(int? id)
+        {
+            // Instantiate HomeDetailsViewModel and store Employee details and PageTitle
+            HomeDetailsViewModel homeDetailsViewModel = new HomeDetailsViewModel()
+            {
+                Employee = _employeeRepository.GetEmployee(id??1),
+                PageTitle = "Employee Details"
+            };
+
+            // Pass the ViewModel object to the View() helper method
+            return View(homeDetailsViewModel);
+        }
+
+       
+
+
+        [HttpGet]
+        public ViewResult Create()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult Create(Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                Employee newEmployee = _employeeRepository.Add(employee);
+                return View();
+
+              //  return RedirectToAction("details", new { id = newEmployee.Id });
+
+
+            }
+
+            return View();
+        }
+
+
+
+    }
+}
